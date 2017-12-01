cdb996d291e6d1ed4a90b99d1bad608531f4718c
kevin-powell <kev@kevinpowelldesign.com>
2017-12-01 08:28:35 -0500

    js modal

---
+++ js/modal.js
@@ -0,0 +1,41 @@
+const portfolioContainer = document.querySelector('.portfolio-items')
+
+portfolioContainer.addEventListener('click', e => {
+  // console.log(e)
+  e.preventDefault()
+
+  const modalToggle = e.target.closest('.portfolio-link')
+  // console.log(modalToggle)
+  if (! modalToggle) return
+
+  const modal = modalToggle.parentNode.nextElementSibling
+  const closeButton = modal.querySelector('.modal-close')
+
+  const modalOpen = _ => {
+    modal.classList.add('is-open')
+    modal.style.animation = 'modalIn 500ms forwards'
+    document.body.style.overflowY = 'hidden'
+  }
+
+  const modalClose = _ => {
+    modal.classList.remove('is-open')
+    modal.removeEventListener('animationend', modalClose)
+  }
+
+  closeButton.addEventListener('click', _ => {
+    modal.style.animation = 'modalOut 500ms forwards'
+    modal.addEventListener('animationend', modalClose)
+    document.body.style.overflowY = 'scroll'
+  })
+
+  document.addEventListener('keydown', e => {
+    if ( e.keyCode === 27 ) {
+      modal.style.animation = 'modalOut 500ms forwards'
+      modal.addEventListener('animationend', modalClose)
+      document.body.style.overflowY = 'scroll'
+    }
+  })
+
+  modalOpen()
+
+})
